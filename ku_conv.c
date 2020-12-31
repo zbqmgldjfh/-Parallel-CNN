a1dc77655232d58e9ec8dc91c8591be6fb3bbba6
zbqmgldjfh <60593969+zbqmgldjfh@users.noreply.github.com>
2020-12-31 11:11:48 +0900

    Create ku_conv.c
    과제 제출용 ver1

---
+++ ku_conv.c
@@ -0,0 +1,405 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <mqueue.h>
+#include <sys/resource.h>
+
+void PrintFunc(int **mat, int len);               // 출력함수
+void makeMatrix(int **matrix, int X, int Y);      // matrix에 랜덤수 삽입
+void CheckXY(int num, int *ch_X, int *ch_Y);      // input숫자 판별기
+void MemFree(int **mat, int len);                 // 할당받은 메모리 영역 반환
+int **Convolution(int *x, int *y, int **mat);     // Convolution filtering
+int ConvolLayer(int (*fun)[3]);                   // Convolution core filter
+int **MaxPooling(int *x, int *y, int **mat);      // max-pooling filtering
+int PoolingLayer(int (*fun)[2]);                  // max-pooling core filter
+int **ResultMat(int x, int y, int (*func)(void)); // 함수로 인자를 받아 matrix만든후 그 matrix주소 반환
+
+volatile int size = 0;
+int **worker_pids;
+
+struct msg_matrix_3x3_st
+{
+    long id;
+    int value[3][3];
+};
+
+struct msg_matrix_2x2_st
+{
+    long id;
+    int value[2][2];
+};
+
+struct msg_ret_st // 결과값 받아오기
+{
+    long id;
+    int value;
+};
+
+void signalhandler()
+{
+}
+
+mqd_t ku_mq_open(char *mqname, int msg_size) // message queue wrapper funtion
+{
+    struct mq_attr attr;
+    mqd_t mq_des;
+    attr.mq_maxmsg = 10;
+    attr.mq_msgsize = msg_size;
+    mq_des = mq_open(mqname, O_CREAT | O_RDWR, 0600, &attr);
+    if (mq_des < 0)
+    {
+        perror("mq_open()");
+        exit(0);
+    }
+    return mq_des;
+}
+
+void ku_mq_send(char *mqname, char *data, int msg_size, unsigned int id)
+{
+    mqd_t mq_des = ku_mq_open(mqname, msg_size);
+    if (mq_send(mq_des, (char *)&data, msg_size, id) == -1)
+    {
+        perror("mq_send");
+        exit(0);
+    }
+    mq_close(mq_des);
+}
+
+void ku_mq_receive(char *mqname, char *data, int msg_size, unsigned int id)
+{
+    unsigned int msgid;
+    mqd_t mq_des = ku_mq_open(mqname, msg_size);
+
+    if (mq_receive(mq_des, (char *)&data, msg_size, &msgid) == -1)
+    {
+        perror("mq_receive");
+        exit(0);
+    }
+
+    mq_close(mq_des);
+}
+
+void cworker_func(int id)
+{
+    struct msg_matrix_3x3_st msg_matrix_3x3;
+    ku_mq_send("/mq_check", (char *)&id, sizeof(int), 0);
+    pause();
+    ku_mq_receive("/mq_mat_3x3", (char *)&msg_matrix_3x3, sizeof(struct msg_matrix_3x3_st), 0);
+
+    int convl_ret = ConvolLayer(msg_matrix_3x3.value);
+
+    struct msg_ret_st msg_ret = {msg_matrix_3x3.id, convl_ret};
+    ku_mq_send("/mq_check", (char *)&id, sizeof(int), 0);
+    pause();
+    ku_mq_send("/mq_ret", (char *)&msg_ret, sizeof(struct msg_ret_st), 0);
+}
+
+void pworker_func(int id)
+{
+    struct msg_matrix_2x2_st msg_matrix_2x2;
+    ku_mq_send("/mq_check", (char *)&id, sizeof(int), 0);
+    pause();
+    ku_mq_receive("/mq_mat_2x2", (char *)&msg_matrix_2x2, sizeof(struct msg_matrix_2x2_st), 0);
+
+    int mpl_ret = PoolingLayer(msg_matrix_2x2.value);
+
+    struct msg_ret_st msg_ret = {msg_matrix_2x2.id, mpl_ret};
+    ku_mq_send("/mq_check", (char *)&id, sizeof(int), 0);
+    pause();
+    ku_mq_send("/mq_ret", (char *)&msg_ret, sizeof(struct msg_ret_st), 0);
+}
+
+/************************************************MAIN************************************************/
+
+int main(int argc, char *argv[])
+{
+    int colLen0 = 0, colLen1 = 0, colLen2 = 0;
+    int X = 0, Y = 0;
+    int **midMat, **endMat;
+
+    if (argc != 2) // 인자가  2개가 아닌 경우 종료
+    {
+        printf("Please input 2 arguments!!\n");
+        return 0;
+    }
+
+    size = atoi(argv[1]); // 정수변환
+
+    CheckXY(size, &X, &Y); // 옳바른 input인지 확인
+
+    int **oriMat = (int **)malloc(sizeof(int *) * X); // X(행) * Y(열) matrix 메모리 할당, 메모리 할당 함수를 따로 만들까도
+    for (int i = 0; i < X; i++)                       // 생각했는대 혹시몰라 수업ppt방식 예시대로 할당
+        oriMat[i] = (int *)malloc(sizeof(int) * Y);
+
+    makeMatrix(oriMat, X, Y); // num차원 matrix 데이터 생성
+    PrintFunc(oriMat, X);
+
+    worker_pids = (int **)malloc(sizeof(int *) * (X - 2));
+    for (int i = 0; i < X - 2; i++)
+    {
+        worker_pids[i] = (int *)malloc(sizeof(int) * (X - 2));
+    }
+
+    for (int i = 0; i < X - 2; i++)
+    {
+        for (int j = 0; j < X - 2; j++)
+        {
+            int child_pid;
+            if ((child_pid = fork()) == 0)
+            {
+                signal(SIGUSR1, signalhandler);
+                cworker_func(i * X + j);
+                if (i < ((X / 2) - 1) && j < ((X / 2) - 1))
+                    pworker_func(i * X + j);
+                return 0;
+            }
+            else
+            {
+                worker_pids[i][j] = child_pid; // 자식의 pid를 matrix로 저장해둠
+            }
+        }
+    }
+
+    colLen0 = X;
+
+    midMat = Convolution(&X, &Y, oriMat); // convolution된후 생성된 matrix의 주소 반환
+    colLen1 = X;
+
+    PrintFunc(midMat, X);
+
+    endMat = MaxPooling(&X, &Y, midMat); // maxpooling된후 생성된 matrix의 주소 반환
+    colLen2 = X;
+
+    PrintFunc(endMat, X); // 최종 결과 출력
+
+    mq_unlink("/mq_mat_3x3"); // 해당 Queue 삭제
+    mq_unlink("/mq_mat_2x2");
+    mq_unlink("/mq_ret");
+    mq_unlink("/mq_check");
+
+    MemFree(oriMat, colLen0); // num차원 matrix 저장공간 free
+    MemFree(midMat, colLen1);
+    MemFree(endMat, colLen2);
+
+    return 0;
+}
+
+/*******************************************************************************************************/
+
+void CheckXY(int num, int *ch_X, int *ch_Y)
+{
+    if ((num & 1) || num <= 0 || num == 2)
+    { // 홀수이거나, 2거나, 0이하라면 종료 ex) a = 2i + 2 (i = 1, 2, 3...)
+        printf("4 이상의 짝수만 입력가능\n");
+        exit(0);
+    }
+    else
+    {
+        *ch_X = num; // 수업에서 X, Y가 같은 정방행렬이라 가정하였음.
+        *ch_Y = num;
+    }
+}
+
+int **Convolution(int *x, int *y, int **mat) // convolution 곱을 수업때 지정한 filter를 이용하여 진행후 queue보내기
+{
+    int m = (*x);
+    int n = (*y);
+    int newLen = n - 2;
+    int check = 0;
+
+    int **resultMat = (int **)malloc(sizeof(int *) * (newLen)); // x(행) * y(열) 차원 행렬 저장공간 생성
+    for (int c = 0; c < (newLen); c++)
+        resultMat[c] = (int *)malloc(sizeof(int) * (newLen));
+
+    for (int i = 0; i < newLen; i++)
+    {
+        for (int j = 0; j < newLen; j++)
+        {
+            ku_mq_receive("/mq_check", (char *)&check, sizeof(int), 0);
+            printf("come %d", check);
+        }
+    }
+
+    for (int i = 0; i < newLen; i++) // newLen * newLen 만큼 반복
+    {
+        for (int j = 0; j < newLen; j++)
+        {
+            struct msg_matrix_3x3_st msg_matrix_3x3;
+            msg_matrix_3x3.id = i * m + j;
+            for (int a = 0; a < 3; a++)
+            {
+                for (int b = 0; b < 3; b++)
+                {
+                    msg_matrix_3x3.value[a][b] = mat[i + a][j + b];
+                }
+            }
+            ku_mq_send("/mq_mat_3x3", (char *)&msg_matrix_3x3, sizeof(struct msg_matrix_3x3_st), 0);
+            kill(worker_pids[i][j], SIGUSR1);
+        }
+    }
+
+    for (int i = 0; i < newLen; i++)
+    {
+        for (int j = 0; j < newLen; j++)
+        {
+            ku_mq_receive("/mq_check", (char *)&check, sizeof(int), 0);
+        }
+    }
+
+    for (int i = 0; i < newLen; i++)
+    {
+        for (int j = 0; j < newLen; j++)
+        {
+            struct msg_ret_st msg_ret;
+            kill(worker_pids[i][j], SIGUSR1);
+            ku_mq_receive("/mq_ret", (char *)&msg_ret, sizeof(struct msg_ret_st), 0);
+            resultMat[msg_ret.id / size][msg_ret.id % size] = msg_ret.value;
+        }
+    }
+    *x = (newLen);
+    *y = (newLen);
+
+    return (int **)resultMat; // 생성된 result matrix의 포인터 반환
+}
+
+int ConvolLayer(int (*fun)[3])
+{
+    int sum = 0;
+
+    for (int i = 0; i < 3; i++)
+    { // 왼쪽 위 첫 모퉁이 시작점
+        for (int j = 0; j < 3; j++)
+        {
+            if (i == 1 && j == 1)
+            {
+                sum += (fun[i][j] * 8); // filter의 정중앙일 경우 X 8
+            }
+            else
+            {
+                sum += (fun[i][j] * -1); // filter의 정중앙이 아닐경우 X -1
+            }
+        }
+    }
+
+    return sum;
+}
+
+int **MaxPooling(int *x, int *y, int **mat)
+{
+    int m = (*x);
+    int n = (*y);
+    int newLen = (m / 2);
+    int check = 0;
+
+    int **resultMat = (int **)malloc(sizeof(int *) * newLen); // x(행) * y(열) 차원 행렬 저장공간 생성
+    for (int c = 0; c < newLen; c++)
+        resultMat[c] = (int *)malloc(sizeof(int) * newLen);
+
+    for (int i = 0; i < newLen - 1; i++)
+    {
+        for (int j = 0; j < newLen - 1; j++)
+        {
+            ku_mq_receive("/mq_check", (char *)&check, sizeof(int), 0);
+        }
+    }
+
+    for (int i = 0; i < newLen - 1; i++)
+    {
+        for (int j = 0; j < newLen - 1; j++)
+        {
+            struct msg_matrix_2x2_st msg_matrix_2x2;
+            msg_matrix_2x2.id = i * m + j;
+            for (int a = 0; a < 2; a++)
+            {
+                for (int b = 0; b < 2; b++)
+                {
+                    msg_matrix_2x2.value[a][b] = mat[2 * i + a][2 * j + b];
+                }
+            }
+            ku_mq_send("/mq_mat_2x2", (char *)&msg_matrix_2x2, sizeof(struct msg_matrix_2x2_st), 0);
+            kill(worker_pids[i][j], SIGUSR1);
+        }
+    }
+
+    for (int i = 0; i < newLen - 1; i++)
+    {
+        for (int j = 0; j < newLen - 1; j++)
+        {
+            ku_mq_receive("/mq_check", (char *)&check, sizeof(int), 0);
+        }
+    }
+
+    for (int i = 0; i < newLen - 1; i++)
+    {
+        for (int j = 0; j < newLen - 1; j++)
+        {
+            struct msg_ret_st msg_ret;
+            kill(worker_pids[i][j], SIGUSR1);
+            ku_mq_receive("/mq_ret", (char *)&msg_ret, sizeof(struct msg_ret_st), 0);
+            resultMat[msg_ret.id / size][msg_ret.id % size] = msg_ret.value;
+        }
+    }
+
+    *x = newLen;
+    *y = newLen;
+
+    return (int **)resultMat; // 행의 길이를 반환
+}
+
+int PoolingLayer(int (*fun)[2])
+{
+    int max = fun[0][0];
+
+    for (int i = 0; i < 2; i++) // 2 X 2 size의 pooling Layer
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            if (fun[i][j] > max)
+                max = fun[i][j];
+        }
+    }
+    return max;
+}
+
+int **ResultMat(int x, int y, int (*func)(void))
+{
+    int **mat = (int **)malloc(sizeof(int *) * (x)); // x(행) * y(열) 차원 행렬 저장공간 생성
+    for (int i = 0; i < (x); i++)
+        mat[i] = (int *)malloc(sizeof(int) * (y));
+
+    for (int i = 0; i < x; i++)
+    {
+        for (int j = 0; j < y; j++)
+        {
+            mat[i][j] = func();
+        }
+    }
+
+    return mat; // matrix 시작 주소 반환
+}
+
+void PrintFunc(int **mat, int len) // 출력함수
+{
+    int i, j;
+    for (i = 0; i < len; i++)
+    {
+        for (j = 0; j < len; j++)
+        {
+            printf("%d ", mat[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void MemFree(int **mat, int len)
+{
+    for (int i = 0; i < len; i++) // num차원 행렬 저장공간 free
+        free(mat[i]);
+    free(mat);
+}
